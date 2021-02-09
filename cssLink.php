e2c1e6e7edfc258a387f74db8f7fe1282407a941
yann tyb <yanntyb.lbc@gmail.com>
2021-02-09 23:27:35 +0100

    Nav barre style

---
+++ cssLink.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="./css/barreNavig.css">
+<link rel="stylesheet" href="./css/body.css">
+<script src="https://kit.fontawesome.com/78e483bd6f.js" crossorigin="anonymous"></script>
---
+++ parts/barreNavig.php
@@ -0,0 +1,13 @@
+<ul id="navig">
+    <li><button id="firstButton"><a class="button" href="./index.php">IMDb</a></button></li>
+    <li><div class="hoverColor" id="secondeButton"><i class="fas fa-bars"></i>  Menu</div></li>
+    <li><div id="selection">
+            <div id="choice">ALL<i class="fas fa-sort-down"></i></div>
+            <input id="input" type="text" placeholder="Search"></li>
+        </div>
+    <li><a class="hoverColor"  id="firstLink" href="#">IMDB pro</a></li>
+    <li><div id="separation"></div></li>
+    <li><a class="hoverColor"  id="secondeLink" href="#">Watch List</a></li>
+    <li><a class="hoverColor"  id="lastLink" href="#">Signe up</a></li>
+</ul>
+<script src="../js/barreNavScript.js"></script>
