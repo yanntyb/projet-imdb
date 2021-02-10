81f8d16c22b040c6aa8fe1038bb7fae7bd98ddd0
yann tyb <yanntyb.lbc@gmail.com>
2021-02-10 15:29:30 +0100

    final 1

--- index.php
+++ index.php
@@ -6,14 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/index.css">
     <?php include "./cssLink.php" ?>
+    <link rel="stylesheet" href="./css/index.css">
     <?php include "./parts/headerScript.php" ?>
 </head>
 <body>
+<!--Nav barre-->
     <?php include "./parts/barreNavig.php" ?>
+
+<!--Carousel-->
     <?php include "./parts/carousel.php" ?>
+
     <h1>Featured Today</h1>
+
+<!--Deuxieme caroussel-->
+
+
+
     <?php include "./parts/footer.php" ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
